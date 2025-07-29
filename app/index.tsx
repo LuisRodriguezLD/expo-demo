9ff9ccdc3989232b11dd95eaae08ba884eea79e8
Luis Rodriguez <luis.rodriguez@endava.com>
2025-07-29 09:51:02 -0500

    simple pokedex

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import {Text, View, SafeAreaView, FlatList, Pressable} from "react-native";
+import {Link} from "expo-router"
+import useSWR from "swr";
+import {fetcher} from "@/utils";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+
+    const {data, isLoading} = useSWR('https://pokeapi.co/api/v2/pokemon', fetcher)
+
+    if (isLoading) return <Text>Loading...</Text>
+
+    return (
+        <SafeAreaView
+            style={{
+                margin: 20
+            }}
+        >
+            <Text style={{
+                fontSize: 30,
+                fontWeight: 'bold'
+            }}>Pokedex</Text>
+
+            <FlatList data={data.results} renderItem={({item}) => {
+                return (
+                    <Link href={`pokemon/${item.name}`} asChild>
+                        <Pressable style={{backgroundColor: "#DEDEDE", marginTop: 6, borderRadius: 6}}>
+                            <Text style={{padding: 16, textTransform: 'capitalize'}}>{item.name}</Text>
+                        </Pressable>
+                    </Link>
+                )
+            }}/>
+
+        </SafeAreaView>
+    );
 }
